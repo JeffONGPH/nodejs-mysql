f8d96a276d200ba9d590f8d57f41324ed0e55d53
Jeff Ong <JeffOng@PEIKs-MacBook-Pro.local>
2018-08-18 19:14:32 -0400

    manager app and readme.md

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ node.js package installations, run following codes in terminal
 - npm install inquirer
 - npm install mysql
 
-1. Customer View - bamazonCustomer.js
- - This part of the app accepts orders from customers and update store's stock quantity for a particular item.
+1. Customer View (bamazonCustomer.js)
+ - This  app accepts orders from customers and update store's stock quantity for a particular item.
 
  - First, the app displays a list of available items for sale, their costs and current inventory.
 ![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/item%20List.png)
@@ -22,5 +22,31 @@ node.js package installations, run following codes in terminal
      - else 
     ![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/insufficient-quantity.png)
     
-Copyright
-Jeff Ong (C) 2018. All Rights Reserved.
+    
+2. Manager View 
+-Build for the manager, this app allows inventory management and update through four actions from its menu list.
+![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/menu.png)
+#1 View all products 
+![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/viewproducts.png)
+
+#2 View Low inventories 
+    -shows items with quantities less than five 
+    ![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/low.png)
+
+#3 Restock inventory 
+    -add to existing items 
+    ![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/add.png)
+
+#4 Add new items 
+    -expand your list of items for sale!
+     ![alt text](https://github.com/JeffONGPH/nodejs-mysql/blob/master/screenshots/newproduct.png)
+     
+     
+     -----End of documentation------
+     
+     Copyright
+     Jeff Ong (C) 2018. All Rights Reserved.
+    
+    
+    
+
--- bamazonManager.js
+++ bamazonManager.js
@@ -9,10 +9,11 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+// List actions 
 function actionPromptList() {
     inquirer.prompt([{
       type: "list",
-      message: "Select from list below what action you would like to complete",
+      message: "Select desired action from menu list",
       choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
       name: "action"
     }, ]).then(function(selection) {
@@ -33,11 +34,12 @@ function actionPromptList() {
           addNewProduct();
           break;
       }
-    }).catch(function(error) {
-      throw error;
+    }).catch(function(err) {
+      throw err;
     });
   };
 
+  //List all items 
 function productsForSale() {
   connection.query('SELECT * FROM products', function(err, result) {
     if (err) throw err;
@@ -55,6 +57,7 @@ function productsForSale() {
   });
 };
 
+//List items with quantity less than 5 
 function viewLowInventory() {
   connection.query("SELECT id, product_name, stock_quantity, department_name, price FROM products  WHERE stock_quantity < 5", function(err, result) {
     if (err) throw err;
@@ -71,12 +74,102 @@ function viewLowInventory() {
   });
 };
 
+// Add to existing inventory 
+function addInventory(){
+  connection.query("SELECT * FROM products", function(err, result){
+  if(err) throw err;
+  var items= [];
+  for(var i=0; i<result.length; i++){
+  items.push(result[i].product_name);
+  }
+
+  inquirer.prompt([{
+    type: "list",
+    name: "product",
+    choices: items,
+    message: "Which item would you like to add inventory?"
+  }, {
+    type: "input",
+    name: "qty",
+    message: "How much would you like to add?",
+    validate: function(value){
+      if(isNaN(value) === false){return true;}
+      else{return false;}
+    }
+    }]).then(function(ans){
+      var inventory;
+      for(var i=0; i<result.length; i++){
+        if(result[i].product_name === ans.product){
+          inventory= result[i].stock_quantity;
+        }
+      }
+      connection.query("UPDATE Products SET ? WHERE ?", [
+        {stock_quantity: inventory + parseInt(ans.qty)},
+        {product_name: ans.product}
+        ], function(err, result){
+          if(err) throw err;
+          console.log("Update succesful");
+        });
+      })
+  });
+}
+
+//Add New Product 
+function addNewProduct(){
+  var departmentsArray= [];
+
+  connection.query("SELECT * FROM products", function(err, result){
+    if(err) throw err;
+    for(var i = 0; i<result.length; i++){
+      departmentsArray.push(result[i].department_name);
+    }
+  })
+
+  inquirer.prompt([{
+    type: "input",
+    name: "product",
+    message: "Product: ",
+    validate: function(value){
+      if(value){return true;}
+      else{return false;}
+    }
+  }, {
+    type: "list",
+    name: "department",
+    message: "Choose department ",
+    choices: departmentsArray
+  }, {
+    type: "input",
+    name: "price",
+    message: "Set Price ",
+    validate: function(value){
+      if(isNaN(value) === false){return true;}
+      else{return false;}
+    }
+  }, {
+    type: "input",
+    name: "quantity",
+    message: "Set Quantity ",
+    validate: function(value){
+      if(isNaN(value) == false){return true;}
+      else{return false;}
+    }
+  }]).then(function(ans){
+    connection.query("INSERT INTO Products SET ?",{
+      product_name: ans.product,
+      department_name: ans.department,
+      Price: ans.price,
+      stock_quantity: ans.quantity
+    }, function(err, result){
+      if(err) throw err;
+      console.log("Item successfully added");
+    })
+  });
+}
 
 
-
-
-connection.connect(function(error) {
+connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
-  actionPromptList()();
+  actionPromptList();
 });
