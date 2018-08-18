436d087e784c87859b0b959c4e02d8a952117ee8
Jeff Ong <JeffOng@PEIKs-MacBook-Pro.local>
2018-08-17 23:21:04 -0400

    inquirer and database update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,29 +9,80 @@ var connection = mysql.createConnection({
  database: "bamazon_db"
 });
 
-connection.connect(function(err){
-    if(err) throw err;
-    connection.query("SELECT * FROM products", function (err, result) {
-        if (err) throw err;
-        //console.log(result);
-        (listItems(result));
-      });
-   
-});
 
-//LIST PRODUCTS AVAILABLE FOR PRUCHASE 
-var listItems = function (items){
-    console.log("welcome shoppers, please empty our inventories")
-    for (var i=0; i<items.length; i++){
-        var listings = "ItemID: " + items[i].id+"\r\n"+
-                        "Product Description: " + items[i].product_name+"\r\n"+
-                        "Department: " + items[i].department_name+"\r\n"+
-                        "Price: $ "+ items[i].price+"\r\n"+
-                        "Current Stock: " + items[i].stock_quantity+
-                        "\r\n";
-        console.log(listings);
+//Start App
+listItems();
+
+function listItems(){
+    connection.connect(function(err){
+        if(err) throw err;
+        connection.query("SELECT * FROM products", function (err, result) {
+            if (err) throw err;
+            //console.log(result);
+            console.log("welcome shoppers, please empty our inventories")
+            for (var i=0; i<result.length; i++){
+                var listings = "ItemID: " + result[i].id+"\r\n"+
+                                "Product Description: " + result[i].product_name+"\r\n"+
+                                "Department: " + result[i].department_name+"\r\n"+
+                                "Price: $ "+ result[i].price+"\r\n"+
+                                "Current Stock: " + result[i].stock_quantity+
+                                "\r\n";
+                console.log(listings);
+            }
+            purchaseItems();
+          });
+          
+    });
+    
+}
+
+
+//PROMPT QUESTIONS //
+function purchaseItems(){
+    inquirer.prompt([
+    {
+        name: "item",
+        type: "input",
+        message: "Enter ID of item you want"
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?"
     }
-};//
+    ]).then(function(answer){
+        //enough stock//
+        connection.query("SELECT id,product_name,price,stock_quantity FROM products WHERE ?",
+        {id: answer.item},  function(err, res){
+            var chosenItem = res[0]
+            var desiredQuantity = answer.quantity
+            if (chosenItem.stock_quantity > desiredQuantity){
+            
+           
+       //update database//
+       connection.query("UPDATE products SET ? WHERE ?",
+       [
+           {
+               stock_quantity: res[0].stock_quantity-desiredQuantity
+           },
+           {
+               id:answer.item
+           }
+       ], function(error) {
+        if (error) throw err;
+      }); 
+       
+    //message to customer//
+    console.log ("You purchased " + desiredQuantity + " of " + chosenItem.product_name + " for $" + chosenItem.price*desiredQuantity)
+            }else{
+                console.log("Insufficient Quantity!")
+            }
+        });
+    }
+    )
+}
+//purchaseItems
 
 
-//PROMP//
+
+
